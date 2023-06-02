1671be244f79d80a00734fed69c602a6e077e2c9
MagnusHutters <43892387+MagnusHutters@users.noreply.github.com>
2023-06-02 13:50:55 +0200

    Create doSendPump.sh

---
+++ doSendPump.sh
@@ -0,0 +1,12 @@
+
+
+MQTT_HOST="localhost"
+MQTT_PORT=1883
+MQTT_USER="emli23"
+MQTT_PASS="bonk22bonk"
+TOPIC="pump"
+
+mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t $TOPIC -m "p"
+
+
+
