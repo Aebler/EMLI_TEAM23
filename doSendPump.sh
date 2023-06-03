310de5e2101d30bd5f43cce6cdeb835d7f02261d
MagnusHutters <43892387+MagnusHutters@users.noreply.github.com>
2023-06-03 14:06:28 +0200

    Update doSendPump.sh

--- doSendPump.sh
+++ doSendPump.sh
@@ -6,7 +6,6 @@ MQTT_USER="emli23"
 MQTT_PASS="bonk22bonk"
 TOPIC="pump"
 
-mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t $TOPIC -m "p"
-
+mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t $TOPIC -m "1"
 
 
