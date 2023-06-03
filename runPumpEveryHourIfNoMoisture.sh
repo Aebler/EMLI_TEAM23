067e80345c2d814fe69a44af73891a2bce797142
MagnusHutters <43892387+MagnusHutters@users.noreply.github.com>
2023-06-03 13:27:23 +0200

    Update runPumpEveryHourIfNoMoisture.sh

--- runPumpEveryHourIfNoMoisture.sh
+++ runPumpEveryHourIfNoMoisture.sh
@@ -7,22 +7,35 @@ MQTT_USER="emli23"
 MQTT_PASS="bonk22bonk"
 
 TOPIC="moisture"
+PUB_TOPIC="pub_topic"
 
-threshold="20"
+threshold="10"
+sleep_interval=60  # Check every minute
+reset_value="RESET"
+
+# Initialize last_pub_time to current time
+last_pub_time=$(date +%s)
 
 # Endless loop
 while true
 do
     # Get the last value from the MQTT topic
-    
-    
     last_value=$(mosquitto_sub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t $TOPIC -C 1)
 
-    # If the last value is less than the threshold, run the script
-    if (( $(echo "$last_value < $threshold" | bc -l) )); then
-        /home/pi/scripts/doSendPump.sh
+    # Get the last message from the publish topic
+    last_pub_message=$(mosquitto_sub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t $PUB_TOPIC -C 1)
+
+    # If the last message from the publish topic is the reset value, reset the last_pub_time
+    if [ "$last_pub_message" == "$reset_value" ]; then
+        last_pub_time=$(date +%s)
     fi
 
-    # Sleep for 1 hour
-    sleep 3600
+    # If the last value is less than the threshold and it has been an hour since the last reset, run the script
+    if awk 'BEGIN {exit !('$last_value' < '$threshold')}' && [ $(date +%s) -ge $((last_pub_time + 3600)) ]; then
+        /home/pi/scripts/doSendPump.sh
+        last_pub_time=$(date +%s)
+    fi
+
+    # Sleep for a minute
+    sleep $sleep_interval
 done
