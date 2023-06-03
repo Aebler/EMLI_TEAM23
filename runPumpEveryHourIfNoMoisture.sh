038dea1d93a21960fda0b3563d4a231907a58c35
MagnusHutters <43892387+MagnusHutters@users.noreply.github.com>
2023-06-03 14:05:21 +0200

    Update runPumpEveryHourIfNoMoisture.sh

--- runPumpEveryHourIfNoMoisture.sh
+++ runPumpEveryHourIfNoMoisture.sh
@@ -7,14 +7,24 @@ MQTT_USER="emli23"
 MQTT_PASS="bonk22bonk"
 
 TOPIC="moisture"
-PUB_TOPIC="pub_topic"
+PUB_TOPIC="pump"
 
-threshold="10"
-sleep_interval=60  # Check every minute
-reset_value="RESET"
+threshold="20"
+sleep_interval=1  # Check every minute
 
-# Initialize last_pub_time to current time
-last_pub_time=$(date +%s)
+reset_message="1"  # Message that will reset the timer
+pump_timestamp_file="/tmp/pump_every_hour_timestamp"  # File to store the timestamp of the last pump
+
+# Initialize pump_timestamp_file
+echo $(date +%s) > $pump_timestamp_file
+
+# Run mosquitto_sub in the background, update pump_timestamp_file when the reset message is published
+mosquitto_sub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t $PUB_TOPIC | while read -r MESSAGE; do
+  if [ "$MESSAGE" == "1" ]; then
+    echo $(date +%s) > $pump_timestamp_file
+    
+  fi
+done &
 
 # Endless loop
 while true
@@ -22,18 +32,13 @@ do
     # Get the last value from the MQTT topic
     last_value=$(mosquitto_sub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t $TOPIC -C 1)
 
-    # Get the last message from the publish topic
-    last_pub_message=$(mosquitto_sub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -t $PUB_TOPIC -C 1)
-
-    # If the last message from the publish topic is the reset value, reset the last_pub_time
-    if [ "$last_pub_message" == "$reset_value" ]; then
-        last_pub_time=$(date +%s)
-    fi
+    # Get the last reset time from the file
+    last_reset_time=$(cat $pump_timestamp_file)
 
     # If the last value is less than the threshold and it has been an hour since the last reset, run the script
-    if awk 'BEGIN {exit !('$last_value' < '$threshold')}' && [ $(date +%s) -ge $((last_pub_time + 3600)) ]; then
+    if awk 'BEGIN {exit !('$last_value' < '$threshold')}' && [ $(date +%s) -ge $((last_reset_time + 5)) ]; then
         /home/pi/scripts/doSendPump.sh
-        last_pub_time=$(date +%s)
+        echo $(date +%s) > $pump_timestamp_file
     fi
 
     # Sleep for a minute
